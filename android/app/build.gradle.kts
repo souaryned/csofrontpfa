1325eb454c11e385776235aa2db0d23bb023dffe
Mahdi <btmehdi33@gmail.com>
2026-05-16 18:19:12 +0100

    fix: build.gradle.kts - suppression flutter_embedding_debug

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -4,15 +4,15 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services") // plugin Google Services FCM
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.example.cso_mobile"   // ✅ doit correspondre à Firebase
+    namespace = "com.example.cso_mobile"
     compileSdk = 36
 
     defaultConfig {
-       applicationId = "com.example.cso_mobile" // ✅ doit correspondre à Firebase
+        applicationId = "com.example.cso_mobile"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
         versionCode = 1
@@ -35,7 +35,7 @@ android {
             isShrinkResources = false
         }
         release {
-            signingConfig = signingConfigs.getByName("debug") // à changer plus tard
+            signingConfig = signingConfigs.getByName("debug")
             isMinifyEnabled = false
             isShrinkResources = false
         }
@@ -47,18 +47,10 @@ flutter {
 }
 
 dependencies {
-    // ⚠️ Desugar pour Java 17
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 
-    // Firebase BOM
     implementation(platform("com.google.firebase:firebase-bom:34.11.0"))
-
-    // Firebase Messaging
     implementation("com.google.firebase:firebase-messaging")
 
-    // AndroidX Core
     implementation("androidx.core:core-ktx:1.12.0")
-
-    // Flutter embedding
-    implementation("io.flutter:flutter_embedding_debug:1.0.0") // si nécessaire
 }
